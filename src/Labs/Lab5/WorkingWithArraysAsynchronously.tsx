239c92087a6e65ce0aa7d6d200c6b91617231c9a
adhu23520 <adhuvaa5@gmail.com>
2024-06-10 22:03:19 +0300

    last push promise

--- src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
+++ src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
@@ -4,11 +4,25 @@ import { FaPencil } from "react-icons/fa6";
 import React, { useState, useEffect } from "react";
 import * as client from "./client";
 import { removeTodo } from "./client";
+import { FaPlusCircle } from "react-icons/fa";
+
 export default function WorkingWithArraysAsynchronously() {
 
 
 
     const [todos, setTodos] = useState<any[]>([]);
+
+    const createTodo = async () => {
+        const todos = await client.createTodo();
+        setTodos(todos);
+    };
+
+    const postTodo = async () => {
+        const newTodo = await client.postTodo({ title: "New Posted Todo", completed: false, });
+        setTodos([...todos, newTodo]);
+    };
+
+
 
     const [errorMessage, setErrorMessage] = useState(null);
     const updateTodo = async (todo: any) => {
@@ -62,7 +76,10 @@ export default function WorkingWithArraysAsynchronously() {
                 <div id="wd-todo-error-message" className="alert alert-danger mb-2 mt-2">{errorMessage}</div>
             )}
 
-            <h4>Todos</h4>
+            <h4>Todos
+                <FaPlusCircle onClick={createTodo} className="text-success float-end fs-3" id="wd-create-todo" />
+                <FaPlusCircle onClick={postTodo} className="text-primary float-end fs-3 me-3" id="wd-post-todo" />
+            </h4>
             <ul className="list-group">
                 {todos.map((todo) => (
                     <li key={todo.id} className="list-group-item">
--- src/Labs/Lab5/client.ts
+++ src/Labs/Lab5/client.ts
@@ -45,6 +45,18 @@ export const deleteTodo = async (todo: any) => {
     return response.data;
   };
 
+  export const createTodo = async () => {
+    const response = await axios.get(`${TODOS_API}/create`);
+    return response.data;
+  };
+
+  export const postTodo = async (todo: any) => {
+    const response = await axios.post(`${TODOS_API}`, todo);
+    return response.data;
+  };
+  
+  
+
   
   
 
